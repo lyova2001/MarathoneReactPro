d8c14e0520fdaeebfff9789a8fbeac6432639cd4
Lyova-FC <66631396+Lyova-Web@users.noreply.github.com>
2021-05-10 15:32:14 +0300

    Настройка работы с css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import s from './App.modules.scss'
+import './custom.css'
+import cn from 'cl'
 
 const App = () => {
     return (
-        <div>
+        <div className={cn(s.header, 'custom')}>
             A new component, some changes
         </div>
     );
