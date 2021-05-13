30149a5f824d6defcd56d3f86cd10fa95cc2d1c0
Lyova-FC <66631396+Lyova-Web@users.noreply.github.com>
2021-05-13 16:02:06 +0300

    one more test

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,7 @@ import cn from 'classnames';
 import s from './App.module.scss';
 
 const App = () => {
-  return;
-
-  <div className={cn(s.header)}>Test</div>;
+  return <div className={cn(s.header)}>Test</div>;
 };
 
 export default App;
