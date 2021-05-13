2256205f5850a751be946ec7f5a0ad589d9453cf
Lyova-FC <66631396+Lyova-Web@users.noreply.github.com>
2021-05-13 15:11:30 +0300

    Фикс tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,7 @@ import s from './App.module.scss'
 const App = () => {
     return (
         <div className={cn(s.header)}>
-           Я не знал, что нельзя было коммитить до этого, и все нужно было делать в другой ветке.
-           (Изменения ради пулл реквеста)
+           Test
         </div>
     );
 }
